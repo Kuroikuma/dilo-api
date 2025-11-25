2f39c49d0d234d079c31ab14469042f6943c418e
KuroiKuma <odiseoj676@gmail.com>
2025-11-24 18:33:08 -0600

    feat: agregar soporte para UserProfileSummary en el módulo ChatKit

--- src/chatkit/chatkit.module.ts
+++ src/chatkit/chatkit.module.ts
@@ -6,15 +6,20 @@ import { ChatKitSessionsController } from './chatkit-sessions.controller';
 import { UserMongoRepository } from '../infrastructure/repositories/user-mongo.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserDocument, UserSchema } from '../infrastructure/database/schemas/user.schema';
+import { UserProfileSummaryMongoRepository } from 'src/user-profile/infrastructure/mongo/repositories/user-profile-summary-mongo.repository';
+import { UserProfileSummaryDocument, UserProfileSummarySchema } from 'src/user-profile/infrastructure/mongo/schema/user-profile-summary.schema';
 
 @Module({
-  imports: [ConfigModule, MongooseModule.forFeature([{ name: UserDocument.name, schema: UserSchema }])],
+  imports: [ConfigModule, MongooseModule.forFeature([{ name: UserDocument.name, schema: UserSchema }]), MongooseModule.forFeature([{ name: UserProfileSummaryDocument.name, schema: UserProfileSummarySchema }])],
   providers: [
     UserMongoRepository,
+    UserProfileSummaryMongoRepository,
     {
       provide: ChatKitSessionsService,
-      useFactory: (userRepo: UserMongoRepository, configService: ConfigService) => new ChatKitSessionsService(configService, userRepo),
-      inject: [UserMongoRepository, ConfigService],
+      useFactory: (configService: ConfigService, userRepo: UserMongoRepository, userProfileSummaryRepo: UserProfileSummaryMongoRepository) => {
+        return new ChatKitSessionsService(configService, userRepo, userProfileSummaryRepo);
+      },  
+      inject: [ConfigService, UserMongoRepository, UserProfileSummaryMongoRepository],
     },
   ],
   controllers: [ChatKitSessionsController],
